fa78a0b362d4d626d315a4d1a5b09edbf8922cf7
ravio <ruben1995@gmail.com>
2017-04-04 09:17:04 -0500

    agrego documentacion a funciones

--- tareas/Ruben/AutomDiff.jl
+++ tareas/Ruben/AutomDiff.jl
@@ -16,26 +16,55 @@ module AD
     export Dual, xdual
 
 
+    doc"""
+        Dual{T<:Real}
+            fun
+            der
+    Definición de los duales, donde lo campos son:
+    fun, una función y der, su derivada.
+    """
+
     type Dual{T<:Real}
         fun :: T
         der :: T
     end
 
 
+    doc"""
+        Dual(f, d)
+            fun = f
+            der = d
+
+    Función que garantiza que las entradas de un dual tengan sean del mismo tipo.
+    Utiliza la funcion promote().
+    """
+
     function Dual(f, d)
         return Dual(promote(f, d)...)
     end
 
 
+    doc"""
+        Dual(a)
+
+    Crea el Dual de un real, la segunda entrada es 0 ya que es la derivada de una constante
+    """
+
     function Dual(a::Real)
         return Dual(a, 0)
     end
 
+    doc"""
+        xdual(x0) -> Dual(x0, 1)
+
+    Representa a la variable independiente, asociada con su derivada (dx = 1)
+    """
 
     function xdual(x0)
             return Dual(x0,1)
     end
 
+    # Se definen las operaciones con duales.
 
     +(a::Dual, b::Dual) = Dual(a.fun + b.fun, a.der + b.der)
     +(a::Dual, b::Real) = Dual(a.fun + b, a.der)
