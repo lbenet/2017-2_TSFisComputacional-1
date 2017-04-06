4456b5a1af2548b982fb205ef4a5c5502e8d2b8c
rene <ren_mora@ciencias.unam.mx>
2017-04-06 16:52:33 -0500

    Primer pull request de la tarea4

--- tareas/rene/AutomDiff.jl
+++ tareas/rene/AutomDiff.jl
@@ -7,7 +7,6 @@ René Mora Maya
 
 29-03-17
 =#
-
 # La siguiente instrucción sirve para *precompilar* el módulo
 __precompile__(true)
 
@@ -58,14 +57,46 @@ Dual(x0)=Dual(x0,0)
 *(a::Real, b::Dual) = Dual( a*b.fun, a*b.der )
 *(a::Dual, b::Real) = Dual( b*a.fun, b*a.der )
 
-/(a::Dual, b::Dual) = Dual( a.fun/b.fun, (a.der*b.fun-a.fun*b.der)/(a.fun*a.fun) )
+/(a::Dual, b::Dual) = Dual( a.fun/b.fun, (a.der*b.fun-a.fun*b.der)/(b.fun*b.fun) )
 /(a::Dual, b::Real) = Dual( a.fun/b, a.der/b )
+/(a::Real, b::Dual) = Dual( a/b.fun, (-a*b.der)/(b.fun*b.fun) )
 
 ^(a::Dual, b::Int64) = Dual( a.fun^b, b*a.fun^(b-1)*a.der )
 
 ==(a::Dual, b::Dual) =a.fun==b.fun && a.der==b.der
 ==(a::Real, b::Dual) =a==b.fun && 0.0==b.der
 ==(a::Dual, b::Real) =a.fun==b && a.der==0.0
+
+import Base:sqrt,cbrt,exp,log,sin,cos,tan,cot,sec,csc,sinh,cosh,tanh,coth,sech,csch,asin,acos,atan,acot,asec,acsc
+
+for (f,fp) = ((:sin,(:(cos(a.fun)))),
+              (:cos,(:(-sin(a.fun)))),
+              (:sqrt,(:(1/(2*sqrt(a.fun))))),
+              (:cbrt,(:(1/(3*cbrt((a.fun)^2))))),
+              (:exp,(:(exp(a.fun)))),
+              (:log,(:((1/(a.fun))))),
+              (:tan,(:(sec(a.fun)^2))),
+              (:cot,(:(-csc(a.fun)^2))),
+              (:sec,(:(sec(a.fun)*tan(a.fun)))),
+              (:csc,(:(-csc(a.fun)*cot(a.fun)))),
+              (:cosh,(:(sinh(a.fun)))),
+              (:sinh,(:(cosh(a.fun)))),
+              (:tanh,(:(sech(a.fun)^2))),
+              (:coth,(:(-csch(a.fun)^2))),
+              (:sech,(:(-sech(a.fun)*tanh(a.fun)))),
+              (:csch,(:(-csch(a.fun)*coth(a.fun)))),
+              (:asin,(:(1/sqrt(1-(a.fun)^2)))),
+              (:acos,(:(-1/sqrt(1-(a.fun)^2)))),
+              (:atan,(:(1/(1+(a.fun)^2)))),
+              (:acot,(:(-1/(1+(a.fun)^2)))),
+              (:asec,(:(1/(a.fun*sqrt((a.fun)^2-1))))),
+              (:acsc,(:(-1/(a.fun*sqrt((a.fun)^2-1))))))
+    ex = quote
+        $f(a::Dual)=Dual($f(a.fun),$fp*a.der)
+    end
+    println(ex)
+    @eval $ex
+end
 
 end
 
