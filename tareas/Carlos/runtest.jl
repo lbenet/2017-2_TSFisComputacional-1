c157a07ae35e7e21c17c8f91d302488f800f99e6
CARLOS REYES <carlos47@ciencias.unam.mx>
2017-04-18 09:39:12 -0500

    TAREA4 CARLOS (#51)
    * primer commit tarea4
    
    * avances en problema1 tarea4
    
    * definida cada funcion trigonométrica con su derivada
    
    * avances en el problema 1 y 2 de tarea4 y correcciones en automdiff
    
    * problema 1 completado
    
    * datos agregados en automdiff y runtest.jl
    
    * problema 2 finalizado
    
    * problema 3 y pregunta adicional completados, tarea finalizada
    
    * correcciones de código y referencias
    
    * corrección en la pregunta adicional

--- tareas/Carlos/runtest.jl
+++ tareas/Carlos/runtest.jl
@@ -68,5 +68,29 @@ epsilon=Dual(2)^4
 @test Dual(12,2)==Dual(12,2)
 @test Dual(12)==Dual(12,0)
 @test Dual(12,0)==Dual(12)
-    
+
+    #agregado para la tarea 4
+end
+@testset "pruebas realizadas" begin
+    @test sqrt(xdual(1))==Dual(1.0,0.5)
+    @test exp(xdual(1))==Dual(exp(1),exp(1))
+    @test log(xdual(1))==Dual(log(1),1.0)
+    @test sin(xdual(0))==Dual(0.0,1.0)
+    @test cos(xdual(0))==Dual(1.0,0.0)
+    @test tan(xdual(1))==Dual(tan(1),sec(1)^2)
+    @test cot(xdual(1))==Dual(cot(1),-csc(1)^2)
+    @test sec(xdual(1))==Dual(sec(1),sec(1)*tan(1))
+    @test csc(xdual(1))==Dual(csc(1),-csc(1)*cot(1))
+    @test sinh(xdual(1))==Dual(sinh(1),cosh(1))
+    @test cosh(xdual(1))==Dual(cosh(1),sinh(1))
+    @test tanh(xdual(1))==Dual(tanh(1),sech(1)^2)
+    @test coth(xdual(1))==Dual(coth(1),-csch(1)^2)
+    @test sech(xdual(1))==Dual(sech(1),-tanh(1)*sech(1))
+    @test csch(xdual(1))==Dual(csch(1),-coth(1)*csch(1))
+    @test asin(xdual(0.5))==Dual(asin(0.5),1/(sqrt(1-(1/2)^2)))
+    @test acos(xdual(0.5))==Dual(acos(0.5),-1/(sqrt(1-(0.5)^2)))
+    @test atan(xdual(0))==Dual(atan(0),1/(1+0^2))
+    @test acot(xdual(0.5))==Dual(acot(0.5),-1/(1+(0.5)^2))
+    @test asec(xdual(2))==Dual(asec(2),1/((2)*(sqrt(((2)^2)-1))))
+    @test acsc(xdual(2))==Dual(acsc(2),-1/((2)*(sqrt(((2)^2)-1))))
 end
