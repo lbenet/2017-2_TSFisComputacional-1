f0edc7310d5c123b228d0d02b25a9952b2950a2e
carlos <carlos47@ciencias.unam.mx>
2017-05-14 18:13:01 -0500

    corrigiendo runtest, problema1 completado

--- tareas/Carlos/runtest_taylor.jl
+++ tareas/Carlos/runtest_taylor.jl
@@ -3,11 +3,13 @@ using Base.Test
 using BD
 #las pruebas necesarias para taylor.
 @testset "pruebas realizadas" begin
-    @test taylor(1,[1])+taylor(2,[1,2])==Taylor(1,[2,2]) #sumas
-    @test taylor(2,[1,1])+taylor(2,[1,1])==Taylor(1,[2,2])
-    @test taylor(1,[1])-taylor(2,[1,2])==Taylor(1,[0,-2])#restas
-    @test taylor(2,[1,1])-taylor(2,[1,1])==Taylor(1,[0,0])
-    @test 3*taylor(3,[10,20,30])==Taylor(2,[30,60,90])
-    @test taylor(4,[2,4,6,8])/2==Taylor(3,[1,2,3,4])# divisiones
-    @test taylor(2,[1,2])==Taylor(1,[1,2]) #igualdad
+    @test Taylor(0,[1])+Taylor(1,[1,2])==Taylor(1,[2,2]) #sumas
+    @test Taylor(1,[1,1])+Taylor(1,[1,1])==Taylor(1,[2,2])
+    @test Taylor(0,[1])-Taylor(1,[1,2])==Taylor(1,[0,-2])#restas
+    @test Taylor(1,[1,1])-Taylor(1,[1,1])==Taylor(1,[0,0])
+    @test 3*Taylor(2,[10,20,30])==Taylor(2,[30,60,90])#producto
+    @test Taylor(1,[1,1])*Taylor(1,[1,1])==Taylor(2,[1,2,1])
+    @test Taylor(3,[2,4,6,8])/2==Taylor(3,[1,2,3,4])# divisiones
+    @test Taylor(2,[1,2,1])/Taylor(1,[1,1])==Taylor(1,[1,1])
+    @test Taylor(1,[1,2])==Taylor(1,[1,2]) #igualdad
 end
