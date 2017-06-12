a3712a9a1be56621a4f75e6bcfa9776f20082e09
Ruben Dario Araiza Acosta <ruben1995@gmail.com>
2017-06-11 21:05:19 -0500

    prueba

--- proys/Ruben/main.jl
+++ proys/Ruben/main.jl
@@ -2,7 +2,7 @@ include("funciones.jl")
 include("Taylor.jl")
 using ADT, PyPlot
 x̄0 = [1.0, 0.0]
-t0, tf = 0.0, 10000.0
+t0, tf = 0.0, 1_000_000.0
 α, β = 10.0, 10.0
 
 t, sol = integradorT2(ψ, x̄0, t0, tf)
